8958d4f4b1c8775d454bf061795588b5a221cf11
elainecostadev <elainerscosta@hotmail.com>
2022-12-02 18:40:46 -0300

    ajustes no settimeout

--- src/context/TableProvider.js
+++ src/context/TableProvider.js
@@ -4,17 +4,17 @@ import TableContext from './TableContext';
 
 // const URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
 const URL = 'https://swapi.py4e.com/api/planets/';
-// const FIVE_SECONDS = 5000;
+const FIVE_SECONDS = 5000;
 
 function TableProvider({ children }) {
-  // const [logoStars, setLogoStars] = useState(false);
+  const [logoStars, setLogoStars] = useState(false);
   const [data, setData] = useState([]);
   const [dataCopy, setDataCopy] = useState([]);
 
   useEffect(() => {
     const fetchPlanets = async () => {
       try {
-        // setLogoStars(true);
+        setLogoStars(true);
         const response = await fetch(URL);
         const dataPlanets = await response.json();
         const one = 1;
@@ -30,10 +30,10 @@ function TableProvider({ children }) {
           }
           return 0;
         });
-        // setTimeout(() => setLogoStars(false), FIVE_SECONDS);
+        setTimeout(() => setLogoStars(false), FIVE_SECONDS);
         setData(orderDataPlanets);
         setDataCopy(orderDataPlanets);
-        // setLogoStars(false);
+        setLogoStars(true);
       } catch (error) {
         console.log(error);
       }
